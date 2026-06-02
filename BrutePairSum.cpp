8d744789c49becbc0d5f84a4a109b30b1e3564ca
rudrakshchamyal <rudrakshchamyal45@gmail.com>
2026-06-02 11:36:08 +0530

    Brute Approack for Pair SUM

---
+++ BrutePairSum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> vec = {2, 8, 11, 15};
+    int target = 10;
+    vector<int> p;
+    bool found = false;
+
+
+    for( int i = 0 ; i < vec.size() ; i++){
+        for( int j = 0 ; j < vec.size() ; j++){
+            if( target == vec[i] + vec[j] ){
+                p.push_back(vec[i]);
+                p.push_back(vec[j]);
+                break;
+            }
+        }
+        if(found){
+            break;
+        }
+    }
+
+    cout << "(" << p[0] << ", " << p[1] << ")" << endl;
+
+
+    return 0;
+}
