65e26c4e2708e20724d22aab2ed3fcbbcd6a963b
rudrakshchamyal <rudrakshchamyal45@gmail.com>
2026-06-12 12:28:44 +0530

    PATTERNS

---
+++ Striver_DSA/Patterns/pattern10.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 4;
+
+    for( int i = 0 ; i < n+2 ; i++){
+
+        for( int j = 0 ; j < i ; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+// Downward Triangle!
+
+    for( int i = 0 ; i < n ; i++){
+
+        for( int j = 0 ; j < n-i ; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
